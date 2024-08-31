1f3fd23ba103480d93977c38914055efe34f5dc9
akroidankur <developerankurhazarika@gmail.com>
2024-08-31 23:05:47 +0530

    angular material setup with dual theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit, Renderer2 } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MaterialModule } from './helper/material/material.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MaterialModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'WeatherApp';
+export class AppComponent implements OnInit {
+  private readonly renderer: Renderer2 = inject(Renderer2);
+  private readonly document: Document = inject(DOCUMENT);
+  
+  constructor(){}
+
+  ngOnInit(): void {
+      this.setDarkTheme();
+  }
+
+  setDarkTheme() {
+    this.renderer.removeClass(this.document.body, 'light');
+    this.renderer.addClass(this.document.body, 'dark');
+  }
+
+  setLightTheme() {
+    this.renderer.removeClass(this.document.body, 'dark');
+    this.renderer.addClass(this.document.body, 'light');
+  }
 }
